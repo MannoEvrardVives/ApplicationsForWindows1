418b95cda90e300134a289ebc43fda9ae95b2104
Manno Evrard <manno.evrard@gmail.com>
2019-12-23 11:43:56 +0100

    added user details

--- VivesRental.GUI/Models/User.cs
+++ VivesRental.GUI/Models/User.cs
@@ -11,6 +11,61 @@ namespace VivesRental.GUI.Models
     class User : INotifyPropertyChanged
     {
 
+        #region Members
+
+        private int id;
+        private string firstName;
+        private string name;
+        private string email;
+        private string phoneNumber;
+
+        #endregion
+
+        #region Properties
+
+        public int Id
+        {
+            get { return id; }
+        }
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                firstName = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                OnPropertyChanged();
+            }
+        }
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set
+            {
+                phoneNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
