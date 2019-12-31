3bf973326e0e4f214eafbf5c2381893df60276bb
Manno Evrard <manno.evrard@gmail.com>
2019-12-31 08:52:38 +0100

    added base viewmodel and view

---
+++ VivesRental.GUI/ViewModels/NewRentalViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using VivesRental.GUI.Contracts;
+
+namespace VivesRental.GUI.ViewModels
+{
+    class NewRentalViewModel : ViewModelBase, IViewModel
+    {
+
+
+    }
+}
